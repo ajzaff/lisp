17553c95a54848f04e71ff7bbad382d097295387
ajzaff <ajzaff@gmail.com>
2022-05-14 01:02:24 -0400

    Freqdict

---
+++ fdmap/fdmap.go
@@ -0,0 +1,53 @@
+// Package fdmap implements a generic frequency dictionary.
+package fdmap
+
+import (
+	"strconv"
+
+	"github.com/ajzaff/lisp"
+)
+
+type Key interface {
+	lisp.IdLit | lisp.IntLit | lisp.StringLit
+}
+
+type FreqMap[T Key] struct {
+	sc   *lisp.TokenScanner
+	err  error
+	data map[T]int
+}
+
+func NewFreqMap[T Key](sc *lisp.TokenScanner) *FreqMap[T] {
+	return &FreqMap[T]{sc: sc, data: make(map[T]int)}
+}
+
+func (m *FreqMap[T]) Scan() bool {
+	res := m.sc.Scan()
+	if _, t, text := m.sc.Token(); t != lisp.Invalid {
+		var lit lisp.Lit
+		switch t {
+		case lisp.Id:
+			lit = lisp.IdLit(text)
+		case lisp.Int:
+			x, _ := strconv.ParseInt(text, 10, 64)
+			lit = lisp.IntLit(x)
+		case lisp.String:
+			lit = lisp.StringLit(text)
+		}
+		if t, ok := lit.(T); ok {
+			m.data[t]++
+		}
+	}
+	return res
+}
+
+func (m *FreqMap[T]) Err() error {
+	if err := m.sc.Err(); err != nil {
+		return err
+	}
+	return m.err
+}
+
+func (m *FreqMap[T]) Get(key T) int {
+	return m.data[key]
+}
