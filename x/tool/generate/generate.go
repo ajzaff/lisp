bef4511bb0778a2cb16f1200b59e1d51a90a958b
ajzaff <ajzaff@gmail.com>
2022-12-19 19:27:16 -0500

    Rename flags to cons*

--- x/tool/generate/generate.go
+++ x/tool/generate/generate.go
@@ -12,27 +12,27 @@ import (
 
 var (
 	seed         = flag.Int64("seed", 1337, "Seed the random number generator with the given seed")
-	timeSeed     = flag.Bool("seedtime", false, "Seed the random number generator with the current time (overrides seed)")
-	exprMaxDepth = flag.Int("expr_max_depth", 2, "Maximum depth of Expr")
+	seedTime     = flag.Bool("seed_time", false, "Seed the random number generator with the current time (overrides seed)")
 	idWeight     = flag.Int("id_weight", 1, "Weight for emitting Id")
 	intWeight    = flag.Int("int_weight", 1, "Weight for emitting Int")
-	exprWeight   = flag.Int("expr_weight", 10, "Weight for emitting Expr")
+	consWeight   = flag.Int("cons_weight", 10, "Weight for emitting Cons")
+	consMaxDepth = flag.Int("cons_max_depth", 2, "Maximum depth of Cons")
 )
 
 func main() {
 	flag.Parse()
 
 	seed := *seed
-	if *timeSeed {
+	if *seedTime {
 		seed = time.Now().UnixNano()
 	}
 
 	r := rand.New(rand.NewSource(seed))
 	g := fuzzutil.NewGenerator(r)
-	g.ConsMaxDepth = *exprMaxDepth
+	g.ConsMaxDepth = *consMaxDepth
 	g.IdWeight = *idWeight
 	g.IntWeight = *intWeight
-	g.ConsWeight = *exprWeight
+	g.ConsWeight = *consWeight
 
 	lisp.StdPrinter(os.Stdout).Print(g.Next())
 }
