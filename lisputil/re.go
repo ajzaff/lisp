8d46bdeed6660c0000fc462c429ad778274dfa3c
ajzaff <ajzaff@gmail.com>
2022-12-19 19:00:45 -0500

    Fix to lisputil re.go IdPattern

--- lisputil/re.go
+++ lisputil/re.go
@@ -2,7 +2,7 @@ package lisputil
 
 // Regular patterns describing Lisp syntactic elements.
 const (
-	IdPattern   = `\p{L}[\p{L}\d]*`
+	IdPattern   = `\p{L}+`
 	IntPattern  = `0|[1-9]\d*`
 	ConsPattern = `[()]`
 	ValPattern  = IdPattern + "|" + IntPattern + "|" + ConsPattern
