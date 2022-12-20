816646aef874788987477cb1cb757fefb2bb1d8d
ajzaff <ajzaff@gmail.com>
2022-12-20 16:11:04 -0500

    Move repl.go to x/tool/repl and fix

---
+++ x/tool/repl/repl.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/ajzaff/lisp"
+)
+
+const (
+	cur  = "> "
+	cont = "... "
+)
+
+func main() {
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT)
+
+	go func() {
+		s := <-ch
+		fmt.Fprintln(os.Stderr, s.String())
+		os.Exit(0)
+	}()
+
+	var ts lisp.TokenScanner
+	var s lisp.NodeScanner
+
+	var sb strings.Builder
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		switch input := strings.TrimSpace(sc.Text()); {
+		case sb.Len() > 0 && input == "":
+			ts.Reset(strings.NewReader(sb.String()))
+			s.Reset(&ts)
+			var nodes []lisp.Node
+			for s.Scan() {
+				nodes = append(nodes, s.Node())
+			}
+			if err := s.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				sb.Reset()
+				continue
+			}
+			for _, n := range nodes {
+				lisp.StdPrinter(os.Stdout).Print(n.Val)
+			}
+			sb.Reset()
+		case input == "quit":
+			return
+		default:
+			sb.WriteString(input)
+			sb.WriteByte('\n')
+		}
+	}
+}
